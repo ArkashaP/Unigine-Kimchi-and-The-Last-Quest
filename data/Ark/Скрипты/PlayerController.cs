a8c8e750a3fca456d88d77ab55c533878a30730f
ArkashaP <arkasha960@gmail.com>
2025-05-01 18:10:03 +0300

    Add new world and assets for Ark project
    - Created new world metadata file for World_Ark.
    - Removed obsolete FBX file.
    - Added new TestCube FBX model and its metadata.
    - Introduced new materials (Материал.004 and Материал.006) with corresponding metadata.
    - Implemented PlayerController component with basic initialization and update methods.
    - Added sky material and texture assets for environmental setup.

---
+++ data/Ark/Скрипты/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unigine;
+
+[Component(PropertyGuid = "4c363e1c44d7021c7201e6dabb71eeeba6bcace7")]
+public class PlayerController : Component
+{
+	void Init()
+	{
+		// write here code to be called on component initialization
+		
+	}
+	
+	void Update()
+	{
+		// write here code to be called before updating each render frame
+		
+	}
+}
