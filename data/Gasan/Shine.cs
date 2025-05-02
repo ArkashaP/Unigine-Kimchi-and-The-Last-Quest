e53fa8891f22c6873b70fce6ba67ce3ee0119849
ArkashaP <arkasha960@gmail.com>
2025-05-02 21:49:37 +0300

    Добавлена механика для выбора игрока и таймера появления батарейки

--- data/Gasan/Shine.cs
+++ data/Gasan/Shine.cs
@@ -7,16 +7,21 @@ using Unigine;
 public class Shine : Component
 {
 	Node ShineBox;
-	Node Cat;
+	public Node Cat; // Добавил модификатор "public" для этой переменной, чтобы можно выбрать в ручную - кто игрок (в Unigine Editor)
 
-	public Random random = new Random();
+	public float randomTimeMax = 2.0f; // Время таймера Max
+	public float randomTimeMin = 0.5f; // Время таймера Min
 	public Node Battery;	
 	public ObjectMeshStatic boxMesh, batteryMesh;
-	public float ifps = Game.IFps;
+	// public float ifps = Game.IFps; // Убрал эту строку, тк IFps это динамическое значение, и оно меняется в зависимости от кол-ва кадров. 
 	public float emissionTimer;
 	public float progress, targetEmission;
 	
-	//[ShowInEditor] 
+	// Добавлено
+	private float randomTime; // Время для работы таймера
+	private float timerCounter = 0f; // Время для работы таймера
+	private bool timerActivated = false; // Активен ли таймер
+
 	private float TRIGGER_DISTANCE = 5.0f; //Триггер дистанции
 	private float EMISSION_DURATION = 5.0f; //Длительность увлеичивания свечения
 	private float MAX_EMISSION = 5.0f;     //максимальное значение свечения
@@ -24,11 +29,10 @@ public class Shine : Component
 	private float LERP_SPEED = 2.0f; 
 	private float currentEmission;	
 
-
 	void Init()
 	{
 		ShineBox = node as ObjectMeshStatic;
-		Cat = World.GetNodeByName("Cat");
+		// Cat = World.GetNodeByName("Cat"); // Убрано, тк кота мы назначаем в Unigine Editor
 		currentEmission = boxMesh.GetMaterialParameterFloat("emission_scale", 0);
 	}
 	
@@ -43,14 +47,14 @@ public class Shine : Component
 		dvec3 direction = Cat.GetWorldDirection(MathLib.AXIS.Y);
 		
 		boxMesh = ShineBox as ObjectMeshStatic;
-		double distance = MathLib.Length(catPos - shineBoxPos);
+		double distance = MathLib.Distance(catPos, shineBoxPos);
 
 		if(distance <= TRIGGER_DISTANCE)
 		{
 			targetEmission = MAX_EMISSION;
 			if(Input.IsKeyDown(Input.KEY.E))
 			{
-				addBattery(Battery, ShineBox);
+				enableTimer(); // Если мы нажали Е, то включаем таймер отчета времени
 			}	
 		}
 		else
@@ -91,13 +95,24 @@ public class Shine : Component
 			Battery.WorldPosition += new dvec3(0f, 0f, 1000f);
 		}
 
+		// Проверяем если дошли до нужного времени
+		if(timerActivated && timerCounter >= randomTime){ // Если таймер активен и время достигло времени randomTime, то...
+			timerActivated = false; // Выключаем таймер 
+			Battery.WorldPosition = ShineBox.WorldPosition + new dvec3(1f, 1f, 0f); // Перемещаем обьект 
+		}
 
+		// Считаем время если включен таймер
+		if(timerActivated) timerCounter += Game.IFps; // При активации таймера, timerCounter начнет считать время
+		
 	}
 
-	private void addBattery(Node obj, Node parent)
+	private void enableTimer()
     {
-		//float randomFloat = (float)(random.Double() * 1.5); // рандомное появление батарейки сделать не поулчилось(((
-		obj.WorldPosition = parent.WorldPosition + new dvec3(1f, 1f, 0f);
+		if(!timerActivated) {
+			timerCounter = 0f; // Сбрасываем счетчик времени (текущее значение таймера)
+			randomTime = MathLib.RandFloat(randomTimeMin, randomTimeMax); // Выбираем рандомное время чтобы потом в это время произошла остановка
+			timerActivated = true; // Включаем таймер
+		};
     }
 
 }
