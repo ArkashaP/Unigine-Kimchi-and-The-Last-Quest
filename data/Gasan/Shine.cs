62d6ec757ac18e8451b43c3bd1d94aa2a94fe83a
ABDURAKHMANOV-G <G.Abdurakhmanov@outlook.com>
2025-05-02 20:04:25 +0300

    Скрипт по свечении станции и появлении батарейки + ее исчесзновение при касании ее пользователем

---
+++ data/Gasan/Shine.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unigine;
+
+[Component(PropertyGuid = "42f51590e8ff3483dc12647fe6630cfba2fcbdcd")]
+public class Shine : Component
+{
+	void Init()
+	{
+		// write here code to be called on component initialization
+		
+	}
+	
+	void Update()
+	{
+		// write here code to be called before updating each render frame
+		
+	}
+}
